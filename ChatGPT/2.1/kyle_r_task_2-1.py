9f85eff7537f074042853b80901915e029c1976f
Kyle Rainey <rainek1@unlv.nevada.edu>
2024-10-06 19:59:01 -0700

    Refactored kyle_r_task_2-1.py
    Created a fixture for generating unique names for the counter.
    
    Split some functionality off into function

--- ChatGPT/2.1/kyle_r_task_2-1.py
+++ ChatGPT/2.1/kyle_r_task_2-1.py
@@ -1,73 +1,65 @@
-# My tests for the flask counter from the tdd lab
-"""
-Test Cases for Counter Web Service
-
-Create a service that can keep a track of multiple counters
-- API must be RESTful - see the status.py file. Following these guidelines, you can make assumptions about
-how to call the web service and assert what it should return.
-- The endpoint should be called /counters
-- When creating a counter, you must specify the name in the path.
-- Duplicate names must return a conflict error code.
-- The service must be able to update a counter by name.
-- The service must be able to read the counter
-"""
-
 import pytest
-
-# we need to import the unit under test - counter
+import uuid
 from src.counter import app 
-
-# we need to import the file that contains the status codes
 from src import status 
 
 @pytest.fixture()
 def client():
-  return app.test_client()
+    return app.test_client()
+
+@pytest.fixture()
+def unique_counter_name():
+    """Generate a unique counter name for testing."""
+    return f"counter-{uuid.uuid4()}"
 
 @pytest.mark.usefixtures("client")
 class TestCounterEndPoints:
     """Test cases for Counter-related endpoints"""
 
-    def test_create_a_counter(self, client):
+    def create_counter(self, client, counter_name):
+        return client.post(f'/counters/{counter_name}')
+
+    def get_counter(self, client, counter_name):
+        return client.get(f'/counters/{counter_name}')
+
+    def update_counter(self, client, counter_name):
+        return client.put(f'/counters/{counter_name}')
+
+    def delete_counter(self, client, counter_name):
+        return client.delete(f'/counters/{counter_name}')
+
+    def test_create_a_counter(self, client, unique_counter_name):
         """It should create a counter"""
-        result = client.post('/counters/foo')
+        result = self.create_counter(client, unique_counter_name)
         assert result.status_code == status.HTTP_201_CREATED     
 
-    def test_duplicate_a_counter(self, client):
+    def test_duplicate_a_counter(self, client, unique_counter_name):
         """It should return an error for duplicates"""
-        result = client.post('/counters/bar')
-        assert result.status_code == status.HTTP_201_CREATED
-        result = client.post('/counters/bar')
+        self.create_counter(client, unique_counter_name)
+        result = self.create_counter(client, unique_counter_name)
         assert result.status_code == status.HTTP_409_CONFLICT
     
-    def test_update_a_counter(self, client):
-        result = client.post('/counters/testingUpdate')
-        assert result.status_code == status.HTTP_201_CREATED
-        original_count = result.json["testingUpdate"]
-        result = client.put('/counters/testingUpdate')
+    def test_update_a_counter(self, client, unique_counter_name):
+        self.create_counter(client, unique_counter_name)
+        result = self.update_counter(client, unique_counter_name)
         assert result.status_code == status.HTTP_200_OK
-        new_count = result.json["testingUpdate"]
-        assert original_count + 1 == new_count
+        new_count = result.json[unique_counter_name]
+        assert new_count == 1  # Assuming the counter increments by 1
 
-    def test_get_counter(self, client):
-        result = client.post('/counters/testingGet')
-        assert result.status_code == status.HTTP_201_CREATED
-        original_count = result.json["testingGet"]
-        result = client.get('/counters/testingGet')
+    def test_get_counter(self, client, unique_counter_name):
+        self.create_counter(client, unique_counter_name)
+        result = self.get_counter(client, unique_counter_name)
         assert result.status_code == status.HTTP_200_OK
-        returned_count = result.json["testingGet"]
-        assert original_count == returned_count
+        assert result.json[unique_counter_name] == 0  # Assuming initial value is 0
 
-    def test_del_counter(self, client):
-        counter_name = '/counters/testingDel'
-        result = client.post(counter_name)
-        assert result.status_code == status.HTTP_201_CREATED
-        result = client.delete(counter_name)
+    def test_del_counter(self, client, unique_counter_name):
+        self.create_counter(client, unique_counter_name)
+        result = self.delete_counter(client, unique_counter_name)
         assert result.status_code == status.HTTP_204_NO_CONTENT
-        result = client.get(counter_name)
+        result = self.get_counter(client, unique_counter_name)
         assert result.status_code == status.HTTP_404_NOT_FOUND
 
     def test_del_counter_not_present(self, client):
-        counter_name = '/counters/testingDelNotPresent'
-        result = client.delete(counter_name);
+        counter_name = f'counter-{uuid.uuid4()}'
+        result = self.delete_counter(client, counter_name)
         assert result.status_code == status.HTTP_404_NOT_FOUND
