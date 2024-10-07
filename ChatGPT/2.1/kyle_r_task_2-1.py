e4ac6f83fd8bb3a7c536a5dfa86ec9096906ae31
Kyle Rainey <rainek1@unlv.nevada.edu>
2024-10-06 20:15:05 -0700

    Added a couple comments

--- ChatGPT/2.1/kyle_r_task_2-1.py
+++ ChatGPT/2.1/kyle_r_task_2-1.py
@@ -1,6 +1,24 @@
+# My tests for the flask counter from the tdd lab
+"""
+Test Cases for Counter Web Service
+Create a service that can keep a track of multiple counters
+- API must be RESTful - see the status.py file. Following these guidelines, you can make assumptions about
+how to call the web service and assert what it should return.
+- The endpoint should be called /counters
+- When creating a counter, you must specify the name in the path.
+- Duplicate names must return a conflict error code.
+- The service must be able to update a counter by name.
+- The service must be able to read the counter
+"""
 import pytest
+
+# used for creating unique counter identifiers, would need to be added to requirements.txt
 import uuid
+
+# we need to import the unit under test - counter
 from src.counter import app 
+
+# we need to import the file that contains the status codes
 from src import status 
 
 @pytest.fixture()
